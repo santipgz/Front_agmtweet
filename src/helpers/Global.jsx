4cc8a98ab64d0ec2ce80bceadb6ab7f668926278
santipgz <santiagopalomo22@gmail.com>
2024-08-18 12:48:06 +0200

    Fix

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const Global =  {
-  url:"https://api-rest-agmtweet.onrender.com/api-agmtweet/api-agmtweet/"
+  url:"https://api-rest-agmtweet.onrender.com/api-agmtweet/"
 }
